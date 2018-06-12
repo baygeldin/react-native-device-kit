60752c18ae4a8834b0606a517d62ac2e2ba66993
Alexander Baygeldin <a.baygeldin@gmail.com>
2018-06-12 19:10:08 +0300

    Fix inject script

--- scripts/cli.js
+++ scripts/cli.js
@@ -4,11 +4,17 @@ const program = require('commander');
 const fs = require('fs');
 const path = require('path');
 const fsExtra = require('fs-extra');
-s;
+
 program
   .description('Injects proprietary MedM DeviceKit SDK into RNDeviceKit build.')
-  .option('--android', 'Path to MedM DeviceKit SDK Android library (*.aar)')
-  .option('--ios', 'Path to MedM DeviceKit SDK iOS framework (*.feamework)')
+  .option(
+    '--android <path>',
+    'Path to MedM DeviceKit SDK Android library (*.aar)'
+  )
+  .option(
+    '--ios <path>',
+    'Path to MedM DeviceKit SDK iOS framework (*.feamework)'
+  )
   .parse(process.argv);
 
 const thisPath = process.cwd();
